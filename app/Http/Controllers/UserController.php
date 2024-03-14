59419daa136311b540dd12b06889df5bff054786
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-03-14 20:35:27 +0700

    Jobsheet 4 - Praktikum 2.3 - Retrieving Aggregates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,8 @@ class UserController extends Controller
         */
 
         //coba akses model UserModel
-        $user = UserModel::where('username','manager9')->firstOrFail(); //ambil semua data dari tabel m_user
+        $user = UserModel::where('level_id',2)->count();
+        //dd($user);//ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
