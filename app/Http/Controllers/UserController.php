dc4e378e59fb66a0fd334da2cbf203022494c795
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-03-14 22:05:37 +0700

    Jobsheet 4 - Praktikum 2.6 - Create, Read, Update, Delete (CRUD)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,20 +8,22 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ]);
-            $user->username = 'manager12';
-            $user->save();
-            $user->wasChanged();//true
-            $user->wasChanged('username'); //true
-            $user->wasChanged(['username','level_id']); //true
-            $user->wasChanged('nama'); //false
-            dd($user->wasChanged(['nama','username'])); //true
+        $user = UserModel::all();
+        return view('user',['data' => $user]);
+        //$user = UserModel::create(
+          //  [
+            //    'username' => 'manager11',
+              //  'nama' => 'Manager11',
+                //'password' => Hash::make('12345'),
+                //'level_id' => 2,
+            //]);
+  //          $user->username = 'manager12';
+    //        $user->save();
+      //      $user->wasChanged();//true
+        //    $user->wasChanged('username'); //true
+          //  $user->wasChanged(['username','level_id']); //true
+            //$user->wasChanged('nama'); //false
+            //dd($user->wasChanged(['nama','username'])); //true
             //$user->isDirty(); //true
             //$user->isDirty('username');//true
             //$user->isDirty('nama');//false
@@ -60,5 +62,44 @@ class UserController extends Controller
         //coba akses model UserModel
         //$user = UserModel::where('level_id',2)->count();
         //dd($user);//ambil semua data dari tabel m_user
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id,
+        ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->level_id = $request->level_id;
+        $user->save();
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,47 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
+//prwaktikum 4
+Route::get('level', [LevelController::class, 'index']);
+
+
+//praktikum 5
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+//praktimum 6
 Route::get('/user', [UserController::class, 'index']);
+
+
+
+//JS4
+Route::get('/user', [UserController::class, 'index'])->name('/user');
+
+Route::get('/user/tambah', [UserController::class, 'tambah'])->name('/user/tambah');
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan'])->name('/user/tambah_simpan');
+
+Route::get('/user/ubah{id}', [UserController::class, 'ubah'])->name('/user/ubah');
+
+Route::put('/user/ubah_simpan{id}', [UserController::class, 'ubah_simpan'])->name('/user/ubah_simpan');
+
+
+Route::get('/user/hapus{id}', [UserController::class, 'hapus'])->name('/user/hapus');
