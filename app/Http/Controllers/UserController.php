9166a443bf8afa2d815faa009c8f99a6a21483a8
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-03-14 21:06:19 +0700

    Jobsheet 4 - Praktikum 2.5 - Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,33 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrCreate(
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+                'level_id' => 2,
+            ]);
+            $user->username = 'manager12';
+            $user->save();
+            $user->wasChanged();//true
+            $user->wasChanged('username'); //true
+            $user->wasChanged(['username','level_id']); //true
+            $user->wasChanged('nama'); //false
+            dd($user->wasChanged(['nama','username'])); //true
+            //$user->isDirty(); //true
+            //$user->isDirty('username');//true
+            //$user->isDirty('nama');//false
+            //$user->isDirty(['nama','username']); //true
+            
+           // $user->isClean();//false
+            //$user->isClean('username');//false
+            //$user->isClean('nama');//true
+            //$user->isClean(['nama','username']);//false
+            //$user->save();
+        //$user->isDirty(); //false
+        //$user->isClean(); //true
+        //dd($user->isDirty());
        /*
         //tambah data user dengan eloquent model
         $data = 
@@ -41,7 +60,5 @@ class UserController extends Controller
         //coba akses model UserModel
         //$user = UserModel::where('level_id',2)->count();
         //dd($user);//ambil semua data dari tabel m_user
-        $user->save();
-        return view('user', ['data' => $user]);
     }
 }
