94411fee5bb643351c133ead5f5bea1f3bd487a4
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-05-04 19:32:13 +0700

    JS 10 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,19 +5,96 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+// use Illuminate\Contracts\Auth\Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
 
-    protected $table = 'm_user'; //mendefinisikan nama tabel yg digunakan oleh model
-    public $timestamps = false;
-    protected $primaryKey = 'user_id'; //mendefinisikan primary key dr tabel yg digunakan
-    protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password'];
-
-    public function level(): BelongsTo
+    public function getJWTIdentifier()
     {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+        return $this->getKey();
     }
-} 
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    protected $table = 'm_users';
+    protected $primaryKey = 'user_id';
+
+    protected $fillable =
+        [
+            'level_id',
+            'username',
+            'nama',
+            'password'
+        ];
+}
+
+// class UserModel extends Model implements Authenticatable
+// {
+//     use HasFactory;
+
+//     protected $table = "m_users";
+//     protected $primaryKey = "user_id";
+
+//     protected $fillable =
+//         [
+//             'level_id',
+//             'username',
+//             'nama',
+//             'password'
+//         ];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     }
+
+//     public function stok(): HasMany
+//     {
+//         return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+//     }
+
+//     public function transaksi(): HasMany
+//     {
+//         return $this->hasMany(PenjualanModel::class, 'user_id', 'user_id');
+//     }
+
+//     public function updatedTransaksi(): HasMany
+//     {
+//         return $this->hasMany(PenjualanModel::class, 'updated_by', 'user_id');
+//     }
+//     public function getAuthIdentifierName()
+//     {
+//         return 'user_id';
+//     }
+
+//     public function getAuthIdentifier()
+//     {
+//         return $this->user_id;
+//     }
+
+//     public function getAuthPassword()
+//     {
+//         return $this->password;
+//     }
+
+//     public function getRememberToken()
+//     {
+//         return $this->remember_token;
+//     }
+
+//     public function setRememberToken($value)
+//     {
+//         $this->remember_token = $value;
+//     }
+
+//     public function getRememberTokenName()
+//     {
+//         return 'remember_token';
+//     }
+// }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
