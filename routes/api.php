b1d8bb8a033111bb89ebd531734144113a674378
Lintang Aprillya Sari <lintangaprillya357@gmail.com>
2024-05-04 20:24:48 +0700

    JS 10 - Praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,12 @@ use App\Http\Controllers\Api\KategoriController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
+
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
